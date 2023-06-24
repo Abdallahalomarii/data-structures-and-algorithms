546e9b128a0382be0022ffb8dc088b8c152f1c2f
abood alomary <abboodoa@gmail.com>
2023-06-24 20:22:19 +0300

    Linked List

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -1,10 +1,113 @@
-﻿namespace LinkedList
+﻿namespace Linkedlists
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                LinkedList myList = new LinkedList();
+                myList.InsertAtFirst(50);
+                myList.InsertAtFirst(54);
+                myList.InsertAtFirst(14);
+                Node? CurrentNode = myList.Head;
+                while (CurrentNode != null)
+                {
+                    Console.WriteLine(CurrentNode.Data);
+                    CurrentNode = CurrentNode.Next;
+                }
+                if (myList.Includes(50))
+                {
+                    Console.WriteLine($"The Node you looking at is found ");
+                }
+                else
+                {
+                    Console.WriteLine($"The Node you looking at is not found");
+                }
+                Console.WriteLine(myList.ToStringList());
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+    public class Node
+    {
+        public int? Data { get; set; }
+        public Node? Next { get; set; }
+        public Node(int data)//20
+        {
+            Data = data; // Data = 20 
+            Next = null;
+        }
+    }
+    public class LinkedList
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+        }
+        public void InsertAtFirst(int data)
+        {
+            Node node = new Node(data);
+            if (Head == null)
+            {
+                Head = node;
+            }
+            else
+            {
+                node.Next = Head;
+                Head = node;
+            }
+        }
+        public bool Includes(int data)
+        {
+            Node includeNode = new Node(data);
+            if (Head == null)
+            {
+                return false;
+            }
+            else
+            {
+                Node currentNode = Head;
+
+                while (currentNode != null)
+                {
+                    if (currentNode.Data == includeNode.Data)
+                    {
+                        return true;
+                    }
+                    currentNode = currentNode.Next;
+                }
+            }
+            return false;
+        }
+        public string ToStringList()
+        {
+            Node listToString = Head;
+            string result = "";
+            if (listToString == null)
+            {
+                return result + "{ Null }";
+            }
+            else
+            {
+                while (listToString != null)
+                {
+                    result += "{ " + listToString.Data.ToString() + " } " + "-> ";
+                    listToString = listToString.Next;
+
+                }
+                return result + "Null";
+            }
         }
     }
 }
---
+++ LinkedList/READMR.md
@@ -0,0 +1,22 @@
+# Linked List CodeChallange 05
+
+- ### Approach & Efficiency
+
+    - What approach did you take? Why? What is the Big O space/time for this approach
+
+        - I take a signle linked list approach to move from the head to the tail and created a class called node which has the data and 
+        the next node to move from node to the next node, and the linked class has two node which is Head and the Head point to the first node in the linked list
+        while the Tail point to the last node in linked list
+
+        - the Big O Notation for the insertion is O(1) because i don't have any iteration, but for the print and the searching it is O(n) where n
+        referes to how many time the while will iterate through my list 
+
+- ### Solution
+
+    - here are some screen shots for my code :
+
+    - ![Code]()
+
+    - ![Visual Output]() 
+
+
