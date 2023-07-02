a6d4c2bc6d10cc040efbc511e7f9adff8473e525
abood alomary <abboodoa@gmail.com>
2023-07-03 01:08:46 +0300

    CC07-Created

---
+++ Challange07-linked-list-kth/linked-list-kth/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace linked_list_kth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
