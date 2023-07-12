8dc05383749dde6998685ae88276ade379687a6e
abood alomary <abboodoa@gmail.com>
2023-07-12 14:45:37 +0300

    setup the cc 10

---
+++ Challenge10-stack-and-queue/Challenge10-stack-and-queue/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Challenge10_stack_and_queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Challenge10-stack-and-queue/Test-stack-and-queue/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Test_stack_and_queue
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
