82e90f2251ee225df9a5f325dca3ffbd1d78c857
abood alomary <abboodoa@gmail.com>
2023-07-13 03:28:32 +0300

    Stacks And Queues Ready

---
+++ Challenge10-stack-and-queue/Challenge10-stack-and-queue/Stack&Queue/Queue.cs
@@ -0,0 +1,92 @@
+﻿using Challenge10_stack_and_queue.Stack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge10_stack_and_queue.Stack_Queue
+{
+    public class Queues
+    {
+        private Node? Front { get; set; }
+        private Node? Back { get; set; }
+        public int Count { get; set; }
+
+        public Queues() 
+        {
+            Front = null;
+            Back = null;
+        }
+
+        public void Enqueue(int data)
+        {
+            Node newNode = new(data);
+            if (Front == null)
+            {
+                Front = newNode;
+                Back = newNode;
+                Count++;
+            }
+            else
+            {
+                Back.Next = newNode;
+                Back = Back.Next;
+                Count++;
+            }
+        }
+
+        public int Dequeue()
+        {
+            if (Front == null)
+            {
+                throw new NullReferenceException("The Queue is empty");
+            }
+            else
+            {
+                Node dequeueNode = Front;
+                Front = Front.Next;
+                Count--;
+                return dequeueNode.Data;
+            }
+        }
+
+        public int Peek()
+        {
+            if (Front == null)
+            {
+                throw new NullReferenceException("The stack is Empty");
+            }
+            else
+            {
+                Node peekNode = Front;
+                return peekNode.Data;
+
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if (Front == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void PrintQueue()
+        {
+            Node printNode = Front;
+            Console.Write("Front->");
+            while (printNode != null)
+            {
+                Console.Write($"{{{printNode.Data}}} -> ");
+                printNode = printNode.Next;
+            }
+            Console.WriteLine("Null");
+        }
+    }
+}
---
+++ Challenge10-stack-and-queue/Challenge10-stack-and-queue/Stack&Queue/Stack.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge10_stack_and_queue.Stack
+{
+    public class Stacks 
+    {
+        public Node? Top { get; set; }
+
+        public int Count { get; set; }
+
+        public Stacks() 
+        {
+            Top = null;
+        }
+
+        public void Push(int data)
+        {
+            Node currentNode = new(data);
+
+            if (Top == null)
+            {
+                Top = currentNode;
+                Count++;
+            }
+            else
+            {
+                currentNode.Next = Top;
+                Top = currentNode;
+                Count++;
+            }
+        }
+
+        public int Pop()
+        {
+            if (Top == null)
+            {
+                throw new NullReferenceException("The Stack is Empty");
+            }
+            else
+            {
+                Node removedNode = Top;
+                Top = Top.Next;
+                Count--;
+                return removedNode.Data;
+                
+            }
+        }
+
+        public int Peek()
+        {
+            if(Top == null)
+            {
+                throw new NullReferenceException("The stack is Empty");
+            }
+            else
+            {
+                // return Top.Data;
+                // Or by creating another Node
+                Node peekNode = Top;
+                return peekNode.Data;
+                
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if (Top == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void PrintStack()
+        {
+            Node printNode = Top;
+            Console.WriteLine("Top");
+            while(printNode != null)
+            {
+                Console.WriteLine($"{{{printNode.Data}}}");
+                printNode = printNode.Next;
+            }
+        }
+
+    }
+}
