44d7606c04a755e9a44795afeea3da8d87651174
abood alomary <abboodoa@gmail.com>
2023-07-26 13:42:12 +0300

    Circular queue

---
+++ DuckDuckGoose/DuckDuckGoose/Queue/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckDuckGoose.Queue
+{
+    public class Node
+    {
+        public string Data { get; set; }
+
+        public Node Next { get; set; }
+
+        public Node(string data) 
+        {
+            Data = data;
+        }
+    }
+}
