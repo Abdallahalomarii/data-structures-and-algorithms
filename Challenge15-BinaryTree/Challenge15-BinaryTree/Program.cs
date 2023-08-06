c4b6e5bc8ca85d98c8f8b88209e6441f62b2e213
Abdallah Alomari <70644811+Abdallahalomarii@users.noreply.github.com>
2023-08-06 12:10:34 +0300

    all solved

--- Challenge15-BinaryTree/Challenge15-BinaryTree/Program.cs
+++ Challenge15-BinaryTree/Challenge15-BinaryTree/Program.cs
@@ -4,23 +4,27 @@
     {
         static void Main(string[] args)
         {
-            
-                BinarySearchTree newTree = new BinarySearchTree();
 
-                newTree.Add(20);
+            BinarySearchTree newTree = new BinarySearchTree();
 
+            newTree.Add(20);
+            newTree.Add(30);
+            newTree.Add(25);
+            newTree.Add(40);
+            newTree.Add(15);
+            newTree.Add(10);
+            newTree.Add(5);
+            newTree.Add(35);
+            newTree.Add(21);
 
-                newTree.Add(30);
-                newTree.Add(25);
-                newTree.Add(40);
-                newTree.Add(15);
+            Console.WriteLine("InOrder Traversal here -----");
+            string result2 = String.Join(",", newTree.InOrder(newTree.Root, new List<int>()));
 
-                newTree.Add(10);
+            Console.WriteLine(result2);
+            Console.WriteLine();
 
             Console.WriteLine("PreOder Traversal here -----");
             string result1 = String.Join(",", newTree.PreOder(newTree.Root, new List<int>()));
-
-
             Console.WriteLine(result1);
             Console.WriteLine();
 
@@ -31,6 +35,9 @@
             Console.WriteLine();
 
             Console.WriteLine(newTree.Contains(34));
+            Console.WriteLine();
+
+            Console.WriteLine($" The Max Value in The Tree is : {newTree.FindMax()}");
 
             Console.WriteLine("\nBreadth First here -----");
             var breadthFirst = String.Join(",",new Program().BreadthFirst(newTree));
