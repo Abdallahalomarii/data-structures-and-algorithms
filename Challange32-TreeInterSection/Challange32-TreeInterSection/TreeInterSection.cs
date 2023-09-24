8c85601115fbe3e49bf3b2eb0b29f7fe56142d20
Abdallah Alomari <70644811+Abdallahalomarii@users.noreply.github.com>
2023-09-24 20:09:52 +0300

    fixed the reurn values And the table of contents

--- Challange32-TreeInterSection/Challange32-TreeInterSection/TreeInterSection.cs
+++ Challange32-TreeInterSection/Challange32-TreeInterSection/TreeInterSection.cs
@@ -10,32 +10,37 @@ namespace Challange32_TreeInterSection
     public class TreeInterSection
     {
 
-        public List<int> Tree_InterSection(BinaryTree<int> tree1, BinaryTree<int> tree2)
+        public HashTable<int, int> Tree_InterSection(BinaryTree<int> tree1, BinaryTree<int> tree2)
         {
-            if (tree1 == null || tree2 == null)
             {
-                return new List<int>();
-            }
-            HashTable<int,int> hashTable = new HashTable<int,int>();
-            List<int> FirstTree = new List<int>();
-            List<int> SecondTree = new List<int>();
-            List<int> Result = new List<int>();
-            tree1.InOrder(tree1.Root, FirstTree);
-            foreach (var item in FirstTree)
-            {
-                hashTable.Set(item, item); // 100 : 100
-            }
-            tree2.InOrder(tree2.Root, SecondTree);
-
-            foreach(var item in SecondTree)
-            {
-                hashTable.Set(item, item); //100: {100,100}
-                if (hashTable.Get(item).Count() > 1)
+                if (tree1 == null || tree2 == null)
                 {
-                    Result.Add(item);
+                    return new HashTable<int, int>();
                 }
+
+                HashTable<int, int> hashTable = new HashTable<int, int>();
+                List<int> FirstTree = new List<int>();
+                List<int> SecondTree = new List<int>();
+                HashTable<int, int> Result = new HashTable<int, int>();
+
+                tree1.InOrder(tree1.Root, FirstTree);
+                foreach (var item in FirstTree)
+                {
+                    hashTable.Set(item, item); // Store values from the first tree
+                }
+
+                tree2.InOrder(tree2.Root, SecondTree);
+
+                foreach (var item in SecondTree)
+                {
+                    if (hashTable.Has(item))
+                    {
+                        Result.Set(item, item); // Store values that exist in both trees
+                    }
+                }
+
+                return Result;
             }
-            return Result;
         }
     }
 }
--- Challange32-TreeInterSection/TreeIntersectionTest/UnitTest1.cs
+++ Challange32-TreeInterSection/TreeIntersectionTest/UnitTest1.cs
@@ -10,18 +10,18 @@ namespace TreeIntersectionTest
             BinaryTree<int> tree1 = new BinaryTree<int>();
             BinaryTree<int> tree2 = new BinaryTree<int>();
 
-            tree1.Insert(5);
-            tree1.Insert(10);
-            tree1.Insert(13);
-
             TreeInterSection treeInterSection = new TreeInterSection();
 
             var result = treeInterSection.Tree_InterSection(tree1, tree2);
 
-            Assert.Equal(new List<int> { }, result);
+            HashTable<int, int> expected = new HashTable<int, int>();
+
+            Assert.Equal(expected.Map , result.Map);
+            
         }
+
         [Fact]
-        public void IfThereAreNoRepetaedNodesInTheTrees()
+        public void NoRepeatedValues()
         {
             BinaryTree<int> tree1 = new BinaryTree<int>();
             BinaryTree<int> tree2 = new BinaryTree<int>();
@@ -34,16 +34,17 @@ namespace TreeIntersectionTest
             tree2.Insert(9);
             tree2.Insert(11);
 
-
             TreeInterSection treeInterSection = new TreeInterSection();
 
             var result = treeInterSection.Tree_InterSection(tree1, tree2);
 
-            Assert.Equal(new List<int> { }, result);
+            HashTable<int, int> expected = new HashTable<int, int>();
 
+            Assert.Equal(expected.Map, result.Map);
         }
+
         [Fact]
-        public void IfThereAreARepeatedNodesInTheTree()
+        public void IfThereAreRepeatedValues()
         {
             BinaryTree<int> tree1 = new BinaryTree<int>();
             BinaryTree<int> tree2 = new BinaryTree<int>();
@@ -61,9 +62,18 @@ namespace TreeIntersectionTest
 
             var result = treeInterSection.Tree_InterSection(tree1, tree2);
 
-            var expected = new List<int> {10, 13 };
+            HashTable<int, int> expected = new HashTable<int, int>();
+            expected.Set(10, 10);
+            expected.Set(13, 13);
 
-            Assert.Equal(expected, result); 
+            Assert.Equal(expected.Map.Length, result.Map.Length);
+            Assert.Equal(expected.Get(10), result.Get(10));
+            Assert.Equal(expected.Get(13), result.Get(13));
+            Assert.True(result.Has(10));
+            Assert.True(result.Has(13));
+            Assert.True(expected.Has(10));
+            Assert.True(expected.Has(13));
         }
+
     }
 }
